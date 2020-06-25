109d08eb5a5b3dbd3746ebcc72a1fc89a5e02a39
thiagorvc <67349151+thiagorvc@users.noreply.github.com>
2020-06-24 22:44:49 -0300

    Update aluguel.sol
    Reajuste e a Valor doAcrescimo

--- aluguel.sol
+++ aluguel.sol
@@ -23,5 +23,28 @@ contract Aluguel
     function valorDoAluguel () public view returns (uint256) {
          return valor;
     }
+  
+    function simulaMulta ( uint256 mesesRestantes, uint256 totalMesesContrato) public view returns (uint256 valorMulta)
+    {
+   
+    valorMulta = valor*numeroMaximoLegaldeAlugueisParaMulta;
+    valorMulta = valorMulta/totalMesesContrato;
+    valorMulta = valorMulta*mesesRestantes;
+    return valorMulta;
+    }
+
+    function reajusteAluguel (uint256 percentualReajuste) public
+    {
+        if(percentualReajuste >20)
+    {
+            percentualReajuste = 20;
+    }
+        
+    uint256 valorDoAcrescimo = 0;
+    valorDoAcrescimo = ((valor*percentualReajuste)/100);
+    valor=valor +valor;
+
+    }
     
+        
 }
